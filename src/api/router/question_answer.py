3a4b31f73da50466ad59cbfc0375ccd6500b67b3
petersenjoern <57611735+petersenjoern@users.noreply.github.com>
2023-10-23 20:14:42 +0200

    refactor: api: disentagle router repo and service (#11)

--- src/api/router/question_answer.py
+++ src/api/router/question_answer.py
@@ -1,29 +1,85 @@
 """Router for question and answer endpoint"""
 
+import weaviate
+
 from contextlib import asynccontextmanager
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
+from llama_index import PromptHelper, ServiceContext, load_indices_from_storage
+from llama_index.response.schema import StreamingResponse
+from indexing import CHUNK_SIZE, EMBED_MODEL
+from language_models import get_llama2
+from llamaindex_storage import set_storage_ctx
+from llama_index.callbacks import (CallbackManager,LlamaDebugHandler)
+from query_engine import initialise_query_engine
+from llama_index.retrievers import VectorIndexRetriever
+
+from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
-import repository.question_answer as repository_question_answer
 import service.question_answer as service_question_answer
 
 router = APIRouter()
 
-query_engines = {}
+
+LLAMA_INDEX_CALLBACKS_API = CallbackManager([LlamaDebugHandler(print_trace_on_end=True)])
+
+# Weaviate
+WEAVIATE_HOST = "localhost"
+WEAVIATE_PORT = 8080
+
+# LLM
+CONTEXT_WINDOW_API = 3500
+NUM_OUTPUT_API = 596
+# Prompt Helper - can help deal with LLM context window token limitations
+PROMPT_HELPER_API = PromptHelper(
+    context_window=CONTEXT_WINDOW_API,
+    num_output=NUM_OUTPUT_API,
+    chunk_overlap_ratio=0.1,
+    chunk_size_limit=CHUNK_SIZE
+)
+
+lifespan_objects = {}
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """FastApi lifespan - code that is executed before the application starts up."""
 
-    query_engines["vector_indices_and_query_engine"] = repository_question_answer.load_indices_and_query_engine()
+    llm = get_llama2(
+        max_new_tokens=NUM_OUTPUT_API,
+        model_temperature=0.1,
+        context_window=CONTEXT_WINDOW_API
+    )
+    service_context = ServiceContext.from_defaults(
+        llm=llm,
+        chunk_size=CHUNK_SIZE,
+        callback_manager=LLAMA_INDEX_CALLBACKS_API,
+        embed_model=EMBED_MODEL,
+        prompt_helper=PROMPT_HELPER_API
+    )
+
+    weaviate_client = weaviate.Client(url=f"http://{WEAVIATE_HOST}:{WEAVIATE_PORT}")
+    storage_context = set_storage_ctx(weaviate_client)
+    storage_indices = load_indices_from_storage(storage_context=storage_context, service_context=service_context)
+
+    vector_index_retriever=VectorIndexRetriever(
+        index=storage_indices[0],
+        similarity_top_k=2,
+        vector_store_query_mode="default", # 'hybrid' only working with weaviate
+        alpha=0.8, # alpha = 0 -> bm25, alpha=1 -> vector search, only working with hybrid
+        service_context=service_context
+    )
+    query_engine = initialise_query_engine(service_context, vector_index_retriever)
+
+    lifespan_objects["llm"] = llm
+    lifespan_objects["query_engine"] = query_engine
+    lifespan_objects["retriver"] = vector_index_retriever
     yield
     # Clean up and release resources
-    query_engines.clear()
+    lifespan_objects.clear()
 
 
 @router.get("/ask")
 async def ask_documents(question: str) -> StreamingResponse:
     """API endpoint to stream query responses"""
 
-    query_engine = query_engines["vector_indices_and_query_engine"]
-    response_iter = repository_question_answer.run_query_with_engine(query_engine, question)
-    return StreamingResponse(service_question_answer.stream_response(response_iter), media_type="text/event-stream")
+    query_engine = lifespan_objects["query_engine"]
+    return StreamingResponse(service_question_answer.answer_question(query_engine, question), media_type="text/event-stream")
